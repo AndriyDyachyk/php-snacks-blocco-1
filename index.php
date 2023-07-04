4420caf3ea345925bd3ddfb7af2e86af0538cc02
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-07-04 17:29:42 +0200

    snack 3 finitop, esercizio completato

--- index.php
+++ index.php
@@ -60,8 +60,50 @@ if(isset($_GET['nome']) && isset($_GET['mail']) && isset($_GET['eta'])){
     if(is_numeric($_GET['eta'])){
         $flage_age = true;
     }
-}
+};
+// FINE SNACK 2
 
+// SNACK 3
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+]
+// FINE SNACK 3
 ?>
 
 <!DOCTYPE html>
@@ -84,6 +126,7 @@ if(isset($_GET['nome']) && isset($_GET['mail']) && isset($_GET['eta'])){
     </div>
     <!-- FINE SNACK 1 -->
     <hr>
+    <!-- SNACK 2 -->
     <div class="container">
         <div class="row">
             <form action="index.php" method="GET">
@@ -105,5 +148,23 @@ if(isset($_GET['nome']) && isset($_GET['mail']) && isset($_GET['eta'])){
             <?php } ?>
         </div>
     </div>
+    <!-- FINE SNACK 2 -->
+    <hr>
+    <!-- SNACK 3 -->
+    <div class="container">
+        <div class="row">
+            <?php foreach($posts as $key=>$post) { ?>
+                <li>
+                    <?php echo $key; ?>
+                    <ul>
+                        <?php foreach($post as $note) { ?>
+                            <li><?php echo $note['title']." - ".$note['author']." - ".$note['text']?></li>
+                        <?php } ?>        
+                    </ul>
+                </li>
+            <?php  } ?>
+        </div>
+    </div>
+    <!-- FINE SNACK 3 -->
 </body>
 </html>
